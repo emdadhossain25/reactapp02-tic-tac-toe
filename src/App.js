beab8eeddc362fa96f5fecb9043c2066d20e95bc
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2025-10-25 20:35:34 +0600

    game complete for tic tac toe using states, props, hooks, closures

--- src/App.js
+++ src/App.js
@@ -7,17 +7,35 @@ function Square({ value, onSquareClick }) {
   );
 }
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null)); // lifting the prop up for overall state of the boards squares
 
   function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return true;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner" + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "0");
+  }
   return (
     <div>
       <>
+        <div className="status">{status}</div>
         <div className="board-row">
           <Square
             value={squares[0]}
@@ -63,4 +81,24 @@ export default function Board() {
       </>
     </div>
   );
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
